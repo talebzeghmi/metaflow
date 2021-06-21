780a35d31315517f91b11ff3162d867d3fe3f167
Taleb Zeghmi <talebz@zillowgroup.com>
2021-06-20 18:34:31 -0700

    iteration

--- workshop/hello_flow.py
+++ workshop/hello_flow.py
@@ -1,15 +1,29 @@
+
 from metaflow import FlowSpec, step
 
-class HelloFlow(FlowSpec):
 
+class HelloFlow(FlowSpec):
     @step
     def start(self):
-        print("hi in start")
+        """
+        This is the 'start' step. All flows must have a step named 'start' that
+        is the first step in the flow.
+        """
+        self.next(self.hello)
+
+    @step
+    def hello(self):
+        print("Hello <TODO>.")
         self.next(self.end)
 
     @step
     def end(self):
-        pass
+        """
+        This is the 'end' step. All flows must have an 'end' step, which is the
+        last step in the flow.
+        """
+        print("HelloFlow is all done.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     HelloFlow()
